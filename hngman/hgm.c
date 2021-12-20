b18d15f386e1a83229dca67d1f33ad6cabfde324
pro.boooooo <pro.boudjemline@gmail.com>
2021-12-20 16:02:27 +0100

    UP tah les oufs

--- hngman/hgm.c
+++ hngman/hgm.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <windows.h>
+
+typedef struct{
+    char p1[10];
+    char p2[10];
+} player;
 
 int nb_tiret(char* tiret){
     int count = 0; int i= 0;
@@ -12,54 +18,57 @@ int nb_tiret(char* tiret){
     return i;
 }
 
-void hangman(char* secret, char* tiret, int i, int size, char* p1, char* p2){ 
+void hangman(char* secret, char* tiret, int i, int size){ 
 
-    char rep1 = '0', rep2 = '0'; int nbTiret = nb_tiret(tiret);
-    printf("\t\t\tNb de tiret = %d\n", nbTiret);
+    int nbTiret = nb_tiret(tiret); char answer1;
+    i = 0;
+    player p; 
+    strcpy(p.p1, "Bilal"); 
+    strcpy(p.p2, "Tiphaine");
 
-    printf("hangman : \n");
-    printf("%s\n", secret);
-    printf("%s\n\n\n", tiret);
+    while(nbTiret > 0){
+        system("CLS");
+        printf("\t\t\tNb de tiret = %d\n", nbTiret);
 
-    printf("[Au tour de %s] > ", p1); scanf("%s", &rep1);
-    
-    while(nbTiret != 0){
-        while(*(secret + i) != '\0'){
-            if(rep1 == *(secret) + i){
-                printf("premier lettre trouver !");
+        printf("Hang Man : \n");
+        printf("Player 1 ==> %s\nPlayer 2 ==> %s\n\n", p.p1, p.p2);
+        printf("answer : %s\n", secret);
+        printf("answer : %s\n\n\n", tiret);
+
+        printf("Reponse de %s : ", p.p1); 
+        scanf("%c", &answer1);
+
+        while(nbTiret == nbTiret){
+            if(answer1 == *(secret + i)){
+                *(tiret + i) = *(secret + i);
                 nbTiret--;
             }
-            
+
             else{
-                printf("ntm\n");
-                return 0;
+                i++;
             }
         }
-        nbTiret--;
     }
 
     free(tiret);
     free(secret);
 }
 
-void tir_Count(char* secret, int size, char* p1, char* p2){
+void tir_Count(char* secret, int size){
 
     printf("\t\t\tTir Count : [OK]\n");
 
     int i = 0; size = 0;
     char* tiret = malloc(sizeof(char) * i);
 
-    if(*(tiret) == NULL){
-        printf("Erreur MALLOC");
-        exit(1);
-    }
+    if(tiret == NULL) return;
 
     while(*(secret + size) != '\0'){
         *(tiret + i) = '-';
         i++; size++;
     }
 
-    hangman(secret, tiret, i, size, p1, p2);
+    hangman(secret, tiret, i, size);
     
 }
 
@@ -67,18 +76,12 @@ int main(){
     int size = 0;
     char* secret = malloc(sizeof(char) * size);
     
-    if(*(secret) == NULL){
-        printf("Erreur MALLOC");
-        exit(1);
-    }
-
-    char* p1[10] = {'B', 'i', 'l', 'a', 'l'}; 
-    char* p2[10] = {'T', 'i', 'p', 'h', 'a', 'i', 'n', 'e'};
+    if(secret == NULL) return 0;
 
     printf("Choisir le mot secret : "); 
     scanf("%[^\n]", secret);
     system("CLS");
 
-    tir_Count(secret, size, p1, p2);
+    tir_Count(secret, size);
     return 0;
 }
